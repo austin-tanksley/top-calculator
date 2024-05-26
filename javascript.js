257608a20236afd06c50e9b1060b1960499aaa78
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-05-26 16:52:50 -0500

    added basic ui

--- javascript.js
+++ javascript.js
@@ -11,3 +11,23 @@ function multiply(a,b) {
 function divide(a,b) {
     return a/b;
 }
+// input variables
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+//create function that calls operator functions
+function operate(num1,num2,operator) {
+    if (operator == 'add'){
+        return add(num1, num2);
+    }
+    else if (operator == 'subtract') {
+        return subtract(num1,num2);
+    }
+    else if (operator == 'multiply') {
+       return multiply(num1, num2);
+    }
+    else if (operator == 'divide') {
+        return divide(num1, num2);
+    }
+}
