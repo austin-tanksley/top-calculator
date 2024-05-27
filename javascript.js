416d33064e9c7dc858efa9c95c0318a7d2bf9e71
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-05-26 21:11:34 -0500

    finished calculator

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const btnNum = document.querySelectorAll('.btn-num');
 const btnOps = document.querySelectorAll('.btn-operator');
 const btnClear = document.querySelector('.btn-clear')
+const btnEqual = document.querySelector('.btn-equals')
 const display = document.querySelector('.displayContent');
 
 
@@ -10,6 +11,7 @@ let num2 = '';
 let operator = '';
 
 let operateEmpty = true;
+let answerState = false;
 
 //output
 let displayContent = '';
@@ -30,16 +32,16 @@ function divide(a,b) {
 
 //create function that calls operator functions
 function operate(num1,num2,operator) {
-    if (operator == 'add'){
+    if (operator == '+'){
         return add(num1, num2);
     }
-    else if (operator == 'subtract') {
+    else if (operator == '-') {
         return subtract(num1,num2);
     }
-    else if (operator == 'multiply') {
+    else if (operator == 'x') {
        return multiply(num1, num2);
     }
-    else if (operator == 'divide') {
+    else if (operator == '÷') {
         return divide(num1, num2);
     }
 }
@@ -47,15 +49,18 @@ function updateDisplay() {
     display.textContent = displayContent
 }
 function populateDisplay(digit){
-    displayContent = displayContent + String(digit);
-    updateDisplay();
+    if (displayContent.length <= 13){
+        displayContent = displayContent + String(digit);
+        updateDisplay();
+    }
 }
 function clearDisplay(){
     displayContent ='';
     updateDisplay();
     
 }
-//when number button is clicked, if operaterator hasnt been taken yet, add digit to num1, if operator is in place then, add digit to num2
+//when number button is clicked, if operaterator hasnt been taken yet, 
+//add digit to num1, if operator is in place then, add digit to num2
 function addDigit(digit) {
     if (operateEmpty == true){
         num1 = num1.concat(digit);
@@ -67,12 +72,18 @@ function addDigit(digit) {
 
 btnNum.forEach((button)=> {
     button.addEventListener('click', () => {
+        if (answerState == true && operateEmpty == true){
+            displayContent = '';
+            updateDisplay();
+            num1 = '';
+            answerState = false;
+        }
         const buttonValue = button.getAttribute('data-num');
         addDigit(buttonValue);
         populateDisplay(buttonValue);
 
-    })
-})
+    });
+});
 
 btnClear.addEventListener('click',()=>{
     clearDisplay();
@@ -80,5 +91,28 @@ btnClear.addEventListener('click',()=>{
     num2 ='';
     operator = '';
     operateEmpty = true;
-})
+    answerState = false;
+});
 
+//feature to add: minus either makes number negative or changes opereator based on current state
+btnOps.forEach((button)=> {
+    button.addEventListener('click', ()=>{
+        const buttonValue = button.getAttribute('data-num');
+        if (operateEmpty == true) {
+            operator = buttonValue;
+            populateDisplay(buttonValue);
+            operateEmpty = false;
+        }
+    });
+});
+
+btnEqual.addEventListener('click',() => {
+   const answer = operate(Number(num1), Number(num2),operator);
+   displayContent = String(answer);
+   updateDisplay();
+   operator = '';
+   operateEmpty = true;
+   num1 = displayContent;
+   num2 = '';
+   answerState = true;
+});
