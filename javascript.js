7a103a06fa4c250941bed2d4eb8793218505191b
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-05-26 20:24:20 -0500

    added some button functionality

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+const btnNum = document.querySelectorAll('.btn-num');
+const btnOps = document.querySelectorAll('.btn-operator');
+const btnClear = document.querySelector('.btn-clear')
+const display = document.querySelector('.displayContent');
+
+
+// input variables
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+let operateEmpty = true;
+
+//output
+let displayContent = '';
+
 //declare main operator functions
 function add(a,b) {
     return a+b;
@@ -11,10 +27,6 @@ function multiply(a,b) {
 function divide(a,b) {
     return a/b;
 }
-// input variables
-let num1 = 0;
-let num2 = 0;
-let operator = '';
 
 //create function that calls operator functions
 function operate(num1,num2,operator) {
@@ -31,3 +43,42 @@ function operate(num1,num2,operator) {
         return divide(num1, num2);
     }
 }
+function updateDisplay() {
+    display.textContent = displayContent
+}
+function populateDisplay(digit){
+    displayContent = displayContent + String(digit);
+    updateDisplay();
+}
+function clearDisplay(){
+    displayContent ='';
+    updateDisplay();
+    
+}
+//when number button is clicked, if operaterator hasnt been taken yet, add digit to num1, if operator is in place then, add digit to num2
+function addDigit(digit) {
+    if (operateEmpty == true){
+        num1 = num1.concat(digit);
+    }
+    else if (operateEmpty == false) {
+        num2 = num2.concat(digit);
+    }
+}
+
+btnNum.forEach((button)=> {
+    button.addEventListener('click', () => {
+        const buttonValue = button.getAttribute('data-num');
+        addDigit(buttonValue);
+        populateDisplay(buttonValue);
+
+    })
+})
+
+btnClear.addEventListener('click',()=>{
+    clearDisplay();
+    num1 = '';
+    num2 ='';
+    operator = '';
+    operateEmpty = true;
+})
+
